1eb9e6945505b65112529d630a9d6117ea532e89
Xydrel <jasonh78@gmail.com>
2019-08-21 12:12:30 -0700

    ADD: Test to validate empty contrustion when a string is not given for init ADD: Test to validate the value returned by the length method

--- StringQAChellenge/main.cpp
+++ StringQAChellenge/main.cpp
@@ -21,10 +21,36 @@ int main()
         MyString result("a");
         auto compare = result == "a";
         std::cout << "Compare result: " << (int)compare << std::endl;
-        CHECK("Testing MyString construction with string literal", result == "a");
+        CHECK("Testing MyString construction with string literal: expected: Succeeded | result: ", result == "a");
     }
 
     //...TODO: Add more tests
+
+	/*
+		Validate the default constructor is initializing an empty MyString type
+	*/
+	{
+		MyString emptyConstTest;
+		const int expectedEmptyStringLen = 0;
+		auto compare = emptyConstTest.length() <= expectedEmptyStringLen;
+		std::cout << "Validate Empty Constructed string:  " << (int)compare << std::endl;
+		CHECK("Testing MyString Empty construction: expected: Succeeded | result: ", emptyConstTest.length() <= expectedEmptyStringLen);
+	}
+
+	/*
+		Validate that the length method is returning the correct size value
+	*/
+	{
+		MyString lengthTest("test");
+		const int expectedCharLen = 4;
+		auto compare = lengthTest.length() == expectedCharLen;
+		std::cout << "Validate length method: " << (int)compare << std::endl;
+		CHECK("Testing MyString test length() is 4: expected: Succeeded | result: ", lengthTest.length() == expectedCharLen);
+	}
+
+
+	std::cout << "Hit a key to exit test application..." << std::endl;
+	std::cin.get();
 
     return 0;
 }
